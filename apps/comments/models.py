0a75555b43cfac999a21543ee114652d9208bc8f
art <artushakov@gmail.com>
2013-03-01 13:15:46 +0400

    fix Person, add migrations

--- apps/comments/models.py
+++ apps/comments/models.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 __author__ = 'art'
 from django.db import models
-from apps.main.models import Person
 from django.contrib.contenttypes import generic
 from django.contrib.contenttypes.models import ContentType
 
@@ -16,7 +15,7 @@ class Comments(models.Model):
     content_type = models.ForeignKey(ContentType, blank=True, null=True, verbose_name="Type")
     object_id = models.PositiveIntegerField()
     content_object = generic.GenericForeignKey('content_type', 'object_id')
-    author = models.ForeignKey(Person, unique=False)
+    author = models.ForeignKey('main.Person', unique=False)
     txt = models.TextField('Текст комментариев')
     created = models.DateTimeField('Создан', auto_now_add=True)
     updated = models.DateTimeField('Изменен', auto_now_add=True, auto_now=True)
