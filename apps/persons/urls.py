544ff930f65a31631e168288c6da0cb453d96ef7
belovajulia <belovajuliaa@gmail.com>
2013-03-02 08:43:48 +0400

    folow->follow

--- apps/persons/urls.py
+++ apps/persons/urls.py
@@ -8,6 +8,6 @@ from apps.persons import views
 urlpatterns = patterns('',
     url(r'^$', views.SearchPerson.as_view()),
     url(r'^search/*$', views.SearchPerson.as_view()),
-    url(r'^folow/*$', views.FolowPerson.as_view()),
+    url(r'^follow/*$', views.FollowPerson.as_view()),
     
 )
