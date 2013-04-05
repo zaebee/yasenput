c6773e5b909b7c380ebb9881c7235b95d221b297
art <artushakov@gmail.com>
2013-04-05 13:06:22 +0400

    fix: загрузка не всех пинов

--- YasenPut/limit_config.py
+++ YasenPut/limit_config.py
@@ -13,7 +13,7 @@ class LIMITS():
         LIKEUSERS_COUNT = 20
         TAGS_COUNT = None 
         
-        POINTS_SEARCH_COUNT = 5 # число точек на стронице поиска точек
+        POINTS_SEARCH_COUNT = 5  # число точек на стронице поиска точек
         POINTS_LIST_COUNT = 10
     
     class COLLECTIONS_LIST():
