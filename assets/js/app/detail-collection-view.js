343e8918599358ace3a3f862e92245e50f36d89d
tenoclock <tenoclock@noteroma.(none)>
2013-04-26 12:37:14 +0400

    fixes in collections

--- assets/js/app/detail-collection-view.js
+++ assets/js/app/detail-collection-view.js
@@ -52,7 +52,15 @@ $(function(){
                         console.log('we select tab-map');
                         if (!view.popupMap) {
                             console.log('inside of if', view);
-                            coords = [view.model.get('allpoints')[point_id].latitude, view.model.get('allpoints')[point_id].longitude];
+                            var latitude = view.model.get('allpoints')[point_id].latitude
+                            if (!view.model.get('allpoints')[point_id].latitude){
+                                latitude = view.model.get('allpoints')[point_id].point.latitude
+                            };
+                            var longitude = view.model.get('allpoints')[point_id].longitude
+                            if (!view.model.get('allpoints')[point_id].latitude){
+                                longitude = view.model.get('allpoints')[point_id].point.longitude
+                            };
+                            coords = [latitude, longitude];
                             view.popupMap = new ymaps.Map('popup-map-1', {
                                 center: coords,
                                 zoom: 14
@@ -143,7 +151,7 @@ $(function(){
                             console.log('inside of if', view);
                             var latitude = view.model.get('allpoints')[point_id].latitude
                             if (!view.model.get('allpoints')[point_id].latitude){
-                                latitude = view.model.get('points')[point_id].point.latitude
+                                latitude = view.model.get('allpoints')[point_id].point.latitude
                             };
                             var longitude = view.model.get('allpoints')[point_id].longitude
                             if (!view.model.get('allpoints')[point_id].latitude){
