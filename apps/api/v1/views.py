caa1f5d591e318fa074276400c74e261b735fc4c
tenoclock <tenoclock@noteroma.(none)>
2013-07-11 16:29:28 +0400

    routes editing

--- apps/api/v1/views.py
+++ apps/api/v1/views.py
@@ -26,6 +26,7 @@ from pymorphy import get_morph
 from django.db import connection
 from datetime import datetime 
 import ast
+from django.http import QueryDict
 
 def JsonHTTPResponse(json):
         return HttpResponse(simplejson.dumps(json), mimetype="application/json")
@@ -676,7 +677,7 @@ class Route(View):
         return JsonHTTPResponse('ok')
 
     def put(self, request, *args, **kwargs):
-        params = request.PUT
+        params = QueryDict(request.body, request.encoding)
         route_dict =  params.dict()
         name = route_dict['name']
         description = route_dict['desc']
@@ -685,6 +686,14 @@ class Route(View):
         coords = ast.literal_eval(route_dict['coords'])
 
         route = MainModels.Routes.objects.get(id=kwargs.get('id'))
+        if route.author != MainModels.Person.objects.get(username=request.user):
+            route = MainModels.Routes.objects.create(name = name, description = description, author=author, coords = route_dict['coords'])
+        else:
+            route.name = name
+            route.description = description
+            route.points_list = points_list
+            route.coords = coords
+
 
         for point in points_list:
             dot = MainModels.Points.objects.get(id=point.get('id'))
