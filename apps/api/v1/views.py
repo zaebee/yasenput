28f79e77c80d171830c67245f0eb5e1e7f02ca3e
tenoclock <tenoclock@noteroma.(none)>
2013-07-25 17:07:15 +0400

    geo IP

--- apps/api/v1/views.py
+++ apps/api/v1/views.py
@@ -246,12 +246,7 @@ class ItemsList(PointsBaseView):
     log = logger
     def get(self, request, *args, **kwargs):
         params = request.GET
-        PRIVATE_IPS_PREFIX = ('10.', '172.', '192.', )
-
-        #GET CLIENT IP:
-        remote_address = request.META.get('REMOTE_ADDR')
-        ip = remote_address
-        #ip = request.META.get('HTTP_X_FORWARDED_FOR')
+        
         
 
         
@@ -305,13 +300,8 @@ class ItemsList(PointsBaseView):
             search_res_sets = CollectionsModels.Collections.search.none()
             self.log.info('Tags search complete (%.2f sec.) tags_ids: %s' % (time.time()-t0, params.get('tags', '')))
 
-        ipgeobases = IPGeoBase.objects.by_ip("212.49.98.48")
-        f = open('ip.txt','ab+')
-        if ipgeobases.exists():
-            ipgeobase = ipgeobases[0]
-            f.write(str(ipgeobase.latitude) +' '+ str(ipgeobase.longitude) + ' ' + str(ip))
         
-        f.close()
+        
             
 
         if params.get('coord_left'):
@@ -321,32 +311,48 @@ class ItemsList(PointsBaseView):
             #top right coords
             ln_right = float(json.loads(params.get('coord_right')).get('ln'))
             lt_right = float(json.loads(params.get('coord_right')).get('lt'))
-            t0 = time.time()
-            search_res_points_list = search_res_points.all().filter(longitude__lte = ln_right).filter(longitude__gte = ln_left).filter(latitude__lte = lt_right).filter(latitude__gte = lt_left)
-            self.log.info('Filtered by coords complete (%.2f sec.) coords: %s/%s' % (time.time()-t0, params.get('coord_left', ''), params.get('coord_right', '')))
-            search_res_sets_list = []
+        else:
+            #GET CLIENT IP:
+            remote_address = request.META.get('REMOTE_ADDR')
+            ip = request.META.get('HTTP_X_FORWARDED_FOR') or remote_address
+            self.log.info('Client IP:' + str(ip))
+            ipgeobases = IPGeoBase.objects.by_ip(ip)
+            if ipgeobases.exists():
+                ipgeobase = ipgeobases[0]
+            else:
+                ipgeobases = IPGeoBase.objects.by_ip("213.176.241.10")
+                ipgeobase = ipgeobases[0]
+            ln_left = ipgeobase.longitude - 0.1
+            ln_right = ipgeobase.longitude + 0.1
+            lt_left = ipgeobase.latitude - 0.1
+            lt_right = ipgeobase.latitude + 0.1
 
-            for collection in search_res_sets.all():
-                points_l = collection.points.all().filter(longitude__lte = ln_right).filter(longitude__gte = ln_left).filter(latitude__lte = lt_right).filter(latitude__gte = lt_left)
-                if len(points_l) > 0:
-                    search_res_sets_list.append(int(collection.id))
-                
-            search_res_routes_list = []
-            for route in search_res_routes.all():
-                points_l = route.points.all().filter(longitude__lte = ln_right).filter(longitude__gte = ln_left).filter(latitude__lte = lt_right).filter(latitude__gte = lt_left)
-                if len(points_l) > 0:
-                    search_res_routes_list.append(int(route.id))
+        t0 = time.time()
+        search_res_points_list = search_res_points.all().filter(longitude__lte = ln_right).filter(longitude__gte = ln_left).filter(latitude__lte = lt_right).filter(latitude__gte = lt_left)
+        self.log.info('Filtered by coords complete (%.2f sec.) coords: %s/%s' % (time.time()-t0, params.get('coord_left', ''), params.get('coord_right', '')))
+        search_res_sets_list = []
+
+        for collection in search_res_sets.all():
+            points_l = collection.points.all().filter(longitude__lte = ln_right).filter(longitude__gte = ln_left).filter(latitude__lte = lt_right).filter(latitude__gte = lt_left)
+            if len(points_l) > 0:
+                search_res_sets_list.append(int(collection.id))
             
-            if ((search_res_points_list.count()) > 0) or (len(search_res_sets_list) > 0) or (len(search_res_routes_list) > 0):
-                if len(search_res_sets_list) == 0:
-                    search_res_sets = none_qs
-                else:
-                    search_res_sets = CollectionsModels.Collections.objects.all().filter(id__in = search_res_sets_list)
-                if len(search_res_routes_list) == 0:
-                    search_res_routes = none_qs
-                else:
-                    search_res_routes = MainModels.Routes.objects.all().filter(id__in = search_res_routes_list)
-                search_res_points = search_res_points_list
+        search_res_routes_list = []
+        for route in search_res_routes.all():
+            points_l = route.points.all().filter(longitude__lte = ln_right).filter(longitude__gte = ln_left).filter(latitude__lte = lt_right).filter(latitude__gte = lt_left)
+            if len(points_l) > 0:
+                search_res_routes_list.append(int(route.id))
+        
+        if ((search_res_points_list.count()) > 0) or (len(search_res_sets_list) > 0) or (len(search_res_routes_list) > 0):
+            if len(search_res_sets_list) == 0:
+                search_res_sets = none_qs
+            else:
+                search_res_sets = CollectionsModels.Collections.objects.all().filter(id__in = search_res_sets_list)
+            if len(search_res_routes_list) == 0:
+                search_res_routes = none_qs
+            else:
+                search_res_routes = MainModels.Routes.objects.all().filter(id__in = search_res_routes_list)
+            search_res_points = search_res_points_list
 
 
         t0 = time.time()
