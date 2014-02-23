2aaf9f7c8451d86c0bdbc1be7aa2b3b18aa0fa04
art <artushakov@gmail.com>
2014-02-24 02:41:51 +0400

    release

---
+++ assets/docs/api.js
@@ -0,0 +1,20 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Yapp.Points.PointListView"
+    ],
+    "modules": [],
+    "allModules": [
+        {
+            "displayName": "Points",
+            "name": "Points",
+            "description": "Submodule for all points functionality"
+        },
+        {
+            "displayName": "Yapp",
+            "name": "Yapp",
+            "description": "Main application module. It's Marionette.Application instance and namespace of all project classes"
+        }
+    ]
+} };
+});
