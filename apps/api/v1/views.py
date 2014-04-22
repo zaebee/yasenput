19769d0603c3d3bf222f6cab332466f58145d6fb
Roma <tenoclock@noteclock.(none)>
2014-04-22 15:12:28 +0400

    prmerge

--- apps/api/v1/views.py
+++ apps/api/v1/views.py
@@ -399,18 +399,18 @@ class ItemsList(PointsBaseView):
                     ipgeobases = IPGeoBase.objects.by_ip("213.176.241.10")
                     ipgeobase = ipgeobases[0]
                     self.log.info('no client ip in base')
-                ln_left = ipgeobase.latitude-0.1
-                ln_right = ipgeobase.latitude-0.1
-                lt_left = ipgeobase.latitude+0.1
-                lt_right = ipgeobase.latitude+0.1
+                ln_left = ipgeobase.latitude-0.01
+                ln_right = ipgeobase.latitude-0.01
+                lt_left = ipgeobase.latitude+0.01
+                lt_right = ipgeobase.latitude+0.01
             else:
                 ipgeobases = IPGeoBase.objects.by_ip("213.176.241.10")
                 ipgeobase = ipgeobases[0]
                 self.log.info('no client ip in base')
-                ln_left = ipgeobase.latitude-0.1
-                ln_right = ipgeobase.latitude-0.1
-                lt_left = ipgeobase.latitude+0.1
-                lt_right = ipgeobase.latitude+0.1
+                ln_left = ipgeobase.latitude-0.01
+                ln_right = ipgeobase.latitude-0.01
+                lt_left = ipgeobase.latitude+0.01
+                lt_right = ipgeobase.latitude+0.01
                 self.log.info(ipgeobase.city)
 
         t0 = time.time()
