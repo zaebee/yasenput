36756e7121027cc4fe09aa750d5d3254a13937ca
zaebee <zaebuntu@gmail.com>
2014-05-31 17:21:07 +0400

    added person admin

--- apps/main/admin.py
+++ apps/main/admin.py
@@ -5,5 +5,21 @@ from apps.photos.models import Photos
 from apps.collections.models import Collections
 from apps.trips.models import *
 
-admin.site.register((HeadDescriptions, Areas, Regions, Photos, Routes, Categories, Events, Points, TypePoints, PointsByUser, Collections, Person, Position, Trips, Blocks))
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('id', 'avatar_admin', 'first_name', 'last_name', 'email', 'city', 'dealer')
+    list_editable = ('dealer',)
+    list_filter = ('dealer', 'city')
+    search_fields = ('email', 'first_name', 'last_name')
+
+    def avatar_admin(self, obj):
+        try:
+            return u'<img src="%s" width="50" height="50">' % obj.avatar.url
+        except:
+            return u'<img src="/static/images/user-unknown.png" width="50" height="50">'
+    avatar_admin.short_description = 'Avatar'
+    avatar_admin.allow_tags = True
+
+
+admin.site.register((HeadDescriptions, Areas, Regions, Photos, Routes, Categories, Events, Points, TypePoints, PointsByUser, Collections, Position, Trips, Blocks))
 admin.site.register(Order)
+admin.site.register(Person, PersonAdmin)
